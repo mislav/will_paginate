a4ebc22c2792ba750c7867f1b62a013b192bb963
pj <pj@1eaa51fe-a21a-0410-9c2e-ae7a00a434c4>
2007-02-06 23:46:30 +0000

    This needs to be namespaced
    git-svn-id: svn://errtheblog.com/svn/plugins/will_paginate@99 1eaa51fe-a21a-0410-9c2e-ae7a00a434c4

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
 require 'will_paginate'
 require 'finder'
 ActionView::Base.send(:include, WillPaginate)
-ActiveRecord::Base.send(:include, Finder)
+ActiveRecord::Base.send(:include, WillPaginate::Finder)
--- lib/finder.rb
+++ lib/finder.rb
@@ -1,25 +1,27 @@
-module Finder
-  def self.included(base)
-    base.extend ClassMethods
-    class << base
-      alias_method_chain :method_missing, :will_paginate
-      define_method(:per_page) { 30 } unless respond_to? :per_page
-    end
-  end
-
-  module ClassMethods
-    def method_missing_with_will_paginate(method_id, *args, &block)
-      unless match = /^paginate/.match(method_id.to_s)
-        return method_missing_without_will_paginate(method_id, *args, &block) 
+module WillPaginate
+  module Finder
+    def self.included(base)
+      base.extend ClassMethods
+      class << base
+        alias_method_chain :method_missing, :will_paginate
+        define_method(:per_page) { 30 } unless respond_to? :per_page
       end
+    end
 
-      options = args.last.is_a?(Hash) ? args.pop : {}
-      page    = options[:page].to_i.zero? ? 1 : options[:page].to_i
-      options.delete(:page)
-      args << options
+    module ClassMethods
+      def method_missing_with_will_paginate(method_id, *args, &block)
+        unless match = /^paginate/.match(method_id.to_s)
+          return method_missing_without_will_paginate(method_id, *args, &block) 
+        end
 
-      with_scope :find => { :offset => (page - 1) * per_page, :limit => per_page } do
-        [send(method_id.to_s.sub(/^paginate/, 'find'), *args), page]
+        options = args.last.is_a?(Hash) ? args.pop : {}
+        page    = options[:page].to_i.zero? ? 1 : options[:page].to_i
+        options.delete(:page)
+        args << options
+
+        with_scope :find => { :offset => (page - 1) * per_page, :limit => per_page } do
+          [send(method_id.to_s.sub(/^paginate/, 'find'), *args), page]
+        end
       end
     end
   end
