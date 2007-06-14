70c5ee2d5b47d744a5ab30ec89a4bd4bddf53699
chris <chris@1eaa51fe-a21a-0410-9c2e-ae7a00a434c4>
2007-06-14 16:45:37 +0000

    will_paginate: Explicitly require files.  People seem to be having issues  with this and rake. [#40 state:resolved]
    git-svn-id: svn://errtheblog.com/svn/plugins/will_paginate@265 1eaa51fe-a21a-0410-9c2e-ae7a00a434c4

--- init.rb
+++ init.rb
@@ -1,17 +1,6 @@
+require 'will_paginate/collection'
+require 'will_paginate/finder'
+require 'will_paginate/view_helpers'
+
 ActiveRecord::Base.send :include, WillPaginate::Finder
 ActionView::Base.send   :include, WillPaginate::ViewHelpers
-
-unless Hash.instance_methods.include? 'slice'
-  Hash.class_eval do
-    # Returns a new hash with only the given keys.
-    def slice(*keys)
-      allowed = Set.new(respond_to?(:convert_key) ? keys.map { |key| convert_key(key) } : keys)
-      reject { |key,| !allowed.include?(key) }
-    end
-
-    # Replaces the hash with only the given keys.
-    def slice!(*keys)
-      replace(slice(*keys))
-    end
-  end
-end
--- test/helper.rb
+++ test/helper.rb
@@ -30,6 +30,21 @@ class ActiveRecordTestCase < Test::Unit::TestCase
   end
 end
 
+unless Hash.instance_methods.include? 'slice'
+  Hash.class_eval do
+    # Returns a new hash with only the given keys.
+    def slice(*keys)
+      allowed = Set.new(respond_to?(:convert_key) ? keys.map { |key| convert_key(key) } : keys)
+      reject { |key,| !allowed.include?(key) }
+    end
+
+    # Replaces the hash with only the given keys.
+    def slice!(*keys)
+      replace(slice(*keys))
+    end
+  end
+end
+
 ActiveRecordTestConnector.setup
 ActionController::Routing::Routes.reload rescue nil
 ActionController::Routing::Routes.draw do |map|
