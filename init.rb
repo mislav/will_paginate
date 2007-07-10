cb625099972c34f6b98d4bfd023584f9f779f782
mislav <mislav@1eaa51fe-a21a-0410-9c2e-ae7a00a434c4>
2007-07-10 15:12:57 +0000

    port Hash#except from Rails trunk
    git-svn-id: svn://errtheblog.com/svn/plugins/will_paginate@288 1eaa51fe-a21a-0410-9c2e-ae7a00a434c4

--- init.rb
+++ init.rb
@@ -9,3 +9,18 @@ class ActiveRecord::Associations::AssociationCollection
   include WillPaginate::Finder::ClassMethods
   alias_method_chain :method_missing, :paginate
 end
+
+unless Hash.instance_methods.include? 'except'
+  Hash.class_eval do
+    # Returns a new hash without the given keys.
+    def except(*keys)
+      rejected = Set.new(respond_to?(:convert_key) ? keys.map { |key| convert_key(key) } : keys)
+      reject { |key,| rejected.include?(key) }
+    end
+ 
+    # Replaces the hash without only the given keys.
+    def except!(*keys)
+      replace(except(*keys))
+    end
+  end
+end
--- lib/will_paginate/finder.rb
+++ lib/will_paginate/finder.rb
@@ -95,7 +95,7 @@ module WillPaginate
         total_entries = unless options[:total_entries]
           unless args.first.is_a? Array
             # count expects (almost) the same options as find
-            count_options = options.reject { |key, value| key == :count or key == :order }
+            count_options = options.except :count, :order
             # merge the hash found in :count
             # this allows you to specify :select, :order, or anything else just for the count query
             count_options.update(options.delete(:count)) if options[:count]
