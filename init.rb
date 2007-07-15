4c059eeb5508a23e7d215c742195103a0f8d5b1d
mislav <mislav@1eaa51fe-a21a-0410-9c2e-ae7a00a434c4>
2007-07-15 19:03:11 +0000

    Will Paginate: association finders are *finally* tackled. Will Paginate now works properly on stable Rails when it comes to association pagination like "user.projects.paginate(:page => 1)". [#54 state:resolved]
    git-svn-id: svn://errtheblog.com/svn/plugins/will_paginate@298 1eaa51fe-a21a-0410-9c2e-ae7a00a434c4

--- init.rb
+++ init.rb
@@ -6,7 +6,16 @@ require 'will_paginate/view_helpers'
 ActionView::Base.send   :include, WillPaginate::ViewHelpers
 ActiveRecord::Base.send :include, WillPaginate::Finder
 
-class ActiveRecord::Associations::AssociationCollection
-  include WillPaginate::Finder::ClassMethods
-  alias_method_chain :method_missing, :paginate
+module ActiveRecord::Associations
+  # to support paginating finders on associations, we have to mix in the
+  # method_missing magic from WillPaginate::Finder::ClassMethods to AssociationProxy
+  # subclasses, but in a different way for Rails 1.2.x and 2.0
+  (AssociationCollection.instance_methods.include?(:create!) ?
+    AssociationCollection : AssociationCollection.subclasses.map(&:constantize)
+  ).push(HasManyThroughAssociation).each do |klass|
+    klass.class_eval do
+      include WillPaginate::Finder::ClassMethods
+      alias_method_chain :method_missing, :paginate
+    end
+  end
 end
