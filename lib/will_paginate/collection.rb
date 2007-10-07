d5dee204d491b0ecbafd97a6f860f9b19fe35b9e
mislav <mislav@1eaa51fe-a21a-0410-9c2e-ae7a00a434c4>
2007-10-07 19:28:46 +0000

    Will Paginate: more dox for WP::Collection, people seem to use it a lot
    git-svn-id: svn://errtheblog.com/svn/plugins/will_paginate@404 1eaa51fe-a21a-0410-9c2e-ae7a00a434c4

--- lib/will_paginate/collection.rb
+++ lib/will_paginate/collection.rb
@@ -4,11 +4,16 @@ module WillPaginate
   # extra properties. Those properites are used by view helpers to generate
   # correct page links.
   #
+  # WillPaginate::Collection also asists in rolling out your own pagination
+  # solutions: see +create+.
+  #
   class Collection < Array
     attr_reader :current_page, :per_page, :total_entries
 
-    # These collection objects are instantiated by ActiveRecord paginating
-    # finders; there is no need to do it manually.
+    # Arguments to this constructor are the current page number, per-page limit
+    # and the total number of entries. The last argument is optional because it
+    # is best to do lazy counting; in other words, count *conditionally* after
+    # populating the collection using the +replace+ method.
     #
     def initialize(page, per_page, total = nil)
       @current_page = page.to_i
@@ -17,6 +22,31 @@ module WillPaginate
       self.total_entries = total if total
     end
 
+    # Just like +new+, but yields the object after instantiation and returns it
+    # afterwards. This is very useful for manual pagination:
+    #
+    #   @entries = WillPaginate::Collection.create(1, 10) do |pager|
+    #     result = Post.find(:all, :limit => pager.per_page, :offset => pager.offset)
+    #     # inject the result array into the paginated collection:
+    #     pager.replace(result)
+    #
+    #     unless pager.total_entries
+    #       # the pager didn't manage to guess the total count, do it manually
+    #       pager.total_entries = Post.count
+    #     end
+    #   end
+    #
+    # The possibilities with this are endless. For another example, here is how
+    # WillPaginate defines pagination on Array instances:
+    #
+    #   Array.class_eval do
+    #     def paginate(page = 1, per_page = 15)
+    #       WillPaginate::Collection.create(page, per_page, size) do |pager|
+    #         pager.replace self[pager.offset, pager.per_page].to_a
+    #       end
+    #     end
+    #   end
+    #
     def self.create(page, per_page, total = nil, &block)
       pager = new(page, per_page, total)
       yield pager
@@ -58,6 +88,18 @@ module WillPaginate
       @total_pages   = (@total_entries / per_page.to_f).ceil
     end
 
+    # This is a magic wrapper for the original Array#replace method. It serves
+    # for populating the paginated collection after initialization.
+    #
+    # Why magic? Because it tries to guess the total number of entries juding
+    # by the size of given array. If it is shorter than +per_page+ limit, then we
+    # know we're on the last page. This trick is very useful for avoiding
+    # unneccessary hits to the database to do the counting after we fetched the
+    # data for the current page.
+    #
+    # However, after using +replace+ you should always test the value of
+    # +total_entries+ and set it to a proper value if it's +nil+. See the example
+    # in +create+.
     def replace(array)
       super
       # The collection is shorter then page limit? Rejoice, because
