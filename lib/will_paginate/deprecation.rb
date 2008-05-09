b2c9e8f04577953957fcde0149844ab23b951b40
Mislav Marohnić <mislav.marohnic@gmail.com>
2008-05-09 04:47:59 +0200

    no mattr_accessor, I don't use ActiveSupport anymore

--- lib/will_paginate/deprecation.rb
+++ lib/will_paginate/deprecation.rb
@@ -1,7 +1,8 @@
 # borrowed from ActiveSupport::Deprecation
 module WillPaginate
   module Deprecation
-    mattr_accessor :debug
+    def self.debug() @debug end
+    def self.debug=(value) @debug = value end
     self.debug = false
 
     # Choose the default warn behavior according to RAILS_ENV.
@@ -34,7 +35,8 @@ module WillPaginate
     end
 
     # Behavior is a block that takes a message argument.
-    mattr_accessor :behavior
+    def self.debug() @behavior end
+    def self.behavior=(value) @behavior = value end
     self.behavior = default_behavior
 
     def self.silence
