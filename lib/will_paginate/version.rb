c5db3e43a530a4da64ac40118e06eb9a63634812
Mislav Marohnić <mislav.marohnic@gmail.com>
2008-06-08 04:33:15 +0200

    bump version number to 2.5.0 and update README

--- lib/will_paginate/version.rb
+++ lib/will_paginate/version.rb
@@ -1,8 +1,8 @@
 module WillPaginate #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 2
-    MINOR = 3
-    TINY  = 1
+    MINOR = 5
+    TINY  = 0
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
