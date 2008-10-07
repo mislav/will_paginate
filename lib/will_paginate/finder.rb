1449867975e6561bf954942c6811ccacf7151d73
Mislav Marohnić <mislav.marohnic@gmail.com>
2008-10-07 11:32:59 +0200

    fixed paginate(:from) with Rails versions < 2.1 (references #244)

--- lib/will_paginate/finder.rb
+++ lib/will_paginate/finder.rb
@@ -184,6 +184,7 @@ module WillPaginate
       # in the database. It relies on the ActiveRecord +count+ method.
       def wp_count(options, args, finder)
         excludees = [:count, :order, :limit, :offset, :readonly]
+        excludees << :from unless ActiveRecord::Calculations::CALCULATIONS_OPTIONS.include?(:from)
 
         # we may be in a model or an association proxy
         klass = (@owner and @reflection) ? @reflection.klass : self
