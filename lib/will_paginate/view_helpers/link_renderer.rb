065fa41c326c1203ad1ccd6d80462ce061bd8e9a
Mislav Marohnić <mislav.marohnic@gmail.com>
2008-10-12 03:03:48 +0200

    remove the cludgy URL generation optimization

--- lib/will_paginate/view_helpers/link_renderer.rb
+++ lib/will_paginate/view_helpers/link_renderer.rb
@@ -84,41 +84,21 @@ module WillPaginate
       # Returns URL params for +page_link_or_span+, taking the current GET params
       # and <tt>:params</tt> option into account.
       def url_for(page)
-        page_one = page == 1
-        unless @url_string and !page_one
-          @url_params = { :escape => false }
-          # page links should preserve GET parameters
-          stringified_merge @url_params, @template.params if @template.request.get?
-          stringified_merge @url_params, @options[:params] if @options[:params]
+        url_params = { :escape => false }
+        # page links should preserve GET parameters
+        stringified_merge url_params, @template.params if @template.request.get?
+        stringified_merge url_params, @options[:params] if @options[:params]
+        
+        if complex = param_name.index(/[^\w-]/)
+          page_param = (defined?(CGIMethods) ? CGIMethods : ActionController::AbstractRequest).
+            parse_query_parameters("#{param_name}=#{page}")
           
-          if complex = param_name.index(/[^\w-]/)
-            page_param = (defined?(CGIMethods) ? CGIMethods : ActionController::AbstractRequest).
-              parse_query_parameters("#{param_name}=#{page}")
-            
-            stringified_merge @url_params, page_param
-          else
-            @url_params[param_name] = page_one ? 1 : 2
-          end
-
-          url = @template.url_for(@url_params)
-          return url if page_one
-          
-          if complex
-            @url_string = url.sub(%r!([?&]#{CGI.escape param_name}=)#{page}!, '\1@')
-            return url
-          else
-            @url_string = url
-            @url_params[param_name] = 3
-            @template.url_for(@url_params).split(//).each_with_index do |char, i|
-              if char == '3' and url[i, 1] == '2'
-                @url_string[i] = '@'
-                break
-              end
-            end
-          end
+          stringified_merge url_params, page_param
+        else
+          url_params[param_name] = page
         end
-        # finally!
-        @url_string.sub '@', page.to_s
+
+        @template.url_for url_params
       end
 
     private
