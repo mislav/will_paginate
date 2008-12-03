9f1738f5c63d5ce227ebc9a55d89d25efd79f6b6
Mislav Marohnić <mislav.marohnic@gmail.com>
2008-12-04 00:23:01 +0100

    fix view tests with Rails 2.2

--- test/lib/view_test_process.rb
+++ test/lib/view_test_process.rb
@@ -42,15 +42,21 @@ class WillPaginate::ViewTestCase < Test::Unit::TestCase
 
       locals = { :collection => collection, :options => options }
 
-      if defined? ActionView::InlineTemplate
-        # Rails 2.1
-        args = [ ActionView::InlineTemplate.new(@view, @template, locals) ]
+      unless @view.respond_to? :render_template
+        # Rails 2.2
+        @html_result = ActionView::InlineTemplate.new(@template).render(@view, locals)
       else
-        # older Rails versions
-        args = [nil, @template, nil, locals]
+        if defined? ActionView::InlineTemplate
+          # Rails 2.1
+          args = [ ActionView::InlineTemplate.new(@view, @template, locals) ]
+        else
+          # older Rails versions
+          args = [nil, @template, nil, locals]
+        end
+
+        @html_result = @view.render_template(*args)
       end
       
-      @html_result = @view.render_template(*args)
       @html_document = HTML::Document.new(@html_result, true, false)
 
       if block_given?
