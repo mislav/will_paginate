a6d5b07def1329ebf26e736d3f00219496bf4369
Mislav Marohnić <mislav.marohnic@gmail.com>
2008-12-12 09:59:09 +0100

    wrap a test that uses Mocha in #uses_mocha block for consistency

--- test/view_test.rb
+++ test/view_test.rb
@@ -192,13 +192,15 @@ class ViewTest < WillPaginate::ViewTestCase
       @html_result
   end
 
-  def test_page_entries_info_with_longer_class_name
-    @template = '<%= page_entries_info collection %>'
-    collection = ('a'..'z').to_a.paginate
-    collection.first.stubs(:class).returns(mock('class', :name => 'ProjectType'))
+  uses_mocha 'class name' do
+    def test_page_entries_info_with_longer_class_name
+      @template = '<%= page_entries_info collection %>'
+      collection = ('a'..'z').to_a.paginate
+      collection.first.stubs(:class).returns(mock('class', :name => 'ProjectType'))
     
-    paginate collection
-    assert @html_result.index('project types'), "expected <#{@html_result.inspect}> to mention 'project types'"
+      paginate collection
+      assert @html_result.index('project types'), "expected <#{@html_result.inspect}> to mention 'project types'"
+    end
   end
 
   def test_page_entries_info_with_single_page_collection
