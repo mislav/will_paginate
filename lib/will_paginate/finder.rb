2bd01d0a347940fd4dfe0b3818aabea11b87680f
Mislav Marohnić <mislav.marohnic@gmail.com>
2009-05-21 23:28:34 +0200

    count_by_sql: don't use table alias with any adapters starting with "oracle"
    Partially resolves the #237 problem. With JDBC, it still remains an issue
    because the adapter shows up as "jdbc", not "oracle" or "oci"

--- lib/will_paginate/finder.rb
+++ lib/will_paginate/finder.rb
@@ -141,7 +141,7 @@ module WillPaginate
             count_query = original_query.sub /\bORDER\s+BY\s+[\w`,\s]+$/mi, ''
             count_query = "SELECT COUNT(*) FROM (#{count_query})"
             
-            unless ['oracle', 'oci'].include?(self.connection.adapter_name.downcase)
+            unless self.connection.adapter_name =~ /^(oracle|oci$)/i
               count_query << ' AS count_table'
             end
             # perform the count query
