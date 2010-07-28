698d803ae95b8c53ce0fa637fdb4e3dc229e9d9a
Santiago Pastorino <santiago@wyeworks.com>
2010-07-28 17:35:31 -0300

    `Object#returning` is deprecated on Rails 2.3 in favor of `Object#tap`

--- lib/will_paginate.rb
+++ lib/will_paginate.rb
@@ -36,7 +36,7 @@ module WillPaginate
 
       # support pagination on associations
       a = ActiveRecord::Associations
-      returning([ a::AssociationCollection ]) { |classes|
+      [ a::AssociationCollection ].tap { |classes|
         # detect http://dev.rubyonrails.org/changeset/9230
         unless a::HasManyThroughAssociation.superclass == a::HasManyAssociation
           classes << a::HasManyThroughAssociation
