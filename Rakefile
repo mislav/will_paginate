836f3d5f43269197563a64ff1fdae7c264a6639d
Mislav Marohnić <mislav.marohnic@gmail.com>
2011-07-29 15:13:43 +0200

    new ci script for travis

--- Rakefile
+++ Rakefile
@@ -1,11 +1,15 @@
 require 'rake/testtask'
 
 desc 'Default: run unit tests.'
-task :default => [:create_database, :test]
+task :default => :test
 
 desc 'Test the will_paginate plugin.'
 Rake::TestTask.new(:test) do |t|
-  t.pattern = 'test/**/*_test.rb'
+  if ENV['DB'] and ENV['DB'] != 'sqlite3'
+    t.pattern = %w[test/finder_test.rb]
+  else
+    t.pattern = 'test/**/*_test.rb'
+  end
   t.libs << 'test'
 end
 
@@ -20,17 +24,5 @@ namespace :test do ||
   Rake::TestTask.new(:db) do |t|
     t.pattern = %w[test/finder_test.rb]
     t.libs << 'test'
-  end
-end
-
-desc 'Create necessary databases'
-task :create_database do |variable|
-  case ENV['DB']
-  when 'mysql', 'mysql2'
-    `mysql -e 'create database will_paginate;'`
-    abort "failed to create mysql database" unless $?.success?
-  when 'postgres'
-    `psql -c 'create database will_paginate;' -U postgres`
-    abort "failed to create postgres database" unless $?.success?
   end
 end
---
+++ test/ci.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+databases = %w[ sqlite3 mysql postgres ]
+
+def announce(name, msg)
+  puts "\n\e[1;33m[#{name}] #{msg}\e[m\n"
+end
+
+def rails_version(gemfile)
+  gemfile =~ /\d[\d.]*$/ ? $& : '2.3'
+end
+
+if ENV['TRAVIS']
+  system "mysql -e 'create database will_paginate;' >/dev/null"
+  abort "failed to create mysql database" unless $?.success?
+  system "psql -c 'create database will_paginate;' -U postgres >/dev/null"
+  abort "failed to create postgres database" unless $?.success?
+end
+
+gemfiles = ['Gemfile']
+gemfiles.concat Dir['test/gemfiles/*'].reject { |f| f.include? '.lock' }
+
+ruby19 = RUBY_VERSION > '1.9'
+ruby19_gemfiles = gemfiles.first
+
+bundler_options = ENV['TRAVIS'] ? '--path vendor/bundle' : ''
+
+gemfiles.each do |gemfile|
+  next if ruby19 and !ruby19_gemfiles.include? gemfile
+  ENV['BUNDLE_GEMFILE'] = gemfile
+  if system %(bundle install #{bundler_options})
+    for db in databases
+      announce "Rails #{rails_version(gemfile)}", "with #{db}"
+      ENV['DB'] = db
+      system %(bundle exec rake)
+    end
+  end
+end
+
