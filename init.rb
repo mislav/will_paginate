005b8711136af3287a40826016ae3a0c3cfa4d06
Mislav Marohnić <mislav.marohnic@gmail.com>
2011-08-04 12:38:58 +0200

    enable will_paginate to load as a plugin

--- init.rb
+++ init.rb
@@ -1,1 +1,18 @@
 require 'will_paginate'
+
+# This is all duplication of what Railtie does, but is necessary because
+# the initializer defined by the Railtie won't ever run when loaded as plugin.
+
+if defined? ActiveRecord::Base
+  WillPaginate::Railtie.setup_activerecord
+end
+
+if defined? ActionController::Base
+  WillPaginate::Railtie.setup_actioncontroller
+end
+
+if defined? ActionView::Base
+  WillPaginate::Railtie.setup_actionview
+end
+
+WillPaginate::Railtie.add_locale_path config
--- lib/will_paginate/railtie.rb
+++ lib/will_paginate/railtie.rb
@@ -5,19 +5,35 @@ module WillPaginate
   class Railtie < Rails::Railtie
     initializer "will_paginate" do |app|
       ActiveSupport.on_load :active_record do
-        require 'will_paginate/active_record'
-        WillPaginate::ActiveRecord.setup
+        WillPaginate::Railtie.setup_activerecord
       end
 
       ActiveSupport.on_load :action_controller do
-        ActionDispatch::ShowExceptions.rescue_responses['WillPaginate::InvalidPage'] = :not_found
+        WillPaginate::Railtie.setup_actioncontroller
       end
 
       ActiveSupport.on_load :action_view do
-        require 'will_paginate/view_helpers/action_view'
-        include WillPaginate::ActionView
+        WillPaginate::Railtie.setup_actionview
       end
 
+      self.class.add_locale_path config
+    end
+
+    def self.setup_activerecord
+      require 'will_paginate/active_record'
+      WillPaginate::ActiveRecord.setup
+    end
+
+    def self.setup_actioncontroller
+      ActionDispatch::ShowExceptions.rescue_responses['WillPaginate::InvalidPage'] = :not_found
+    end
+
+    def self.setup_actionview
+      require 'will_paginate/view_helpers/action_view'
+      ::ActionView::Base.send :include, WillPaginate::ActionView
+    end
+
+    def self.add_locale_path(config)
       locale_path = File.expand_path('../locale', __FILE__)
       config.i18n.railties_load_path.concat Dir["#{locale_path}/*.{rb,yml}"]
     end
