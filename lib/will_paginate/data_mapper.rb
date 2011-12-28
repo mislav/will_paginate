0bb3c8da9c15910124e8ce446195262fec634d76
pnomolos <pnomolos@gmail.com>
2011-12-28 12:54:30 -0800

    Data Mapper: fix stack overflow

--- lib/will_paginate/data_mapper.rb
+++ lib/will_paginate/data_mapper.rb
@@ -51,7 +51,9 @@ module WillPaginate
           if loaded? and @array.size < per_page and (current_page == 1 or @array.size > 0)
             offset + @array.size
           else
-            clean_query = query.merge(:order => [])
+            # :reload prevents Collection.filter from being run, which
+            # would cause a stack overflow
+            clean_query = query.merge(:reload => true)
             # seems like the only way
             clean_query.instance_variable_set('@limit', nil)
             clean_query.instance_variable_set('@offset', 0)
