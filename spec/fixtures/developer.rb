5aca25978ba1ba0143a5010f5192ec05765ddb04
Mislav Marohnić <mislav.marohnic@gmail.com>
2014-06-18 11:16:07 +0700

    Fix `with_scope` test for Rails 4.1

--- spec/fixtures/developer.rb
+++ spec/fixtures/developer.rb
@@ -2,7 +2,8 @@ class Developer < User
   has_and_belongs_to_many :projects, :order => 'projects.name', :join_table => 'developers_projects'
 
   def self.with_poor_ones(&block)
-    with_scope :find => { :conditions => ['salary <= ?', 80000], :order => 'salary' } do
+    options = { :conditions => ['salary <= ?', 80000], :order => 'salary' }
+    with_scope({ :find => options }, :overwrite) do
       yield
     end
   end
