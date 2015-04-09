55a5b582268474073139e8f22aa72783fdae9da5
Michael Quaas <michael.quaas@betterplace.org>
2015-04-09 09:55:56 +0200

    PageNumber now sanitizes a string starting with a number.

--- lib/will_paginate/page_number.rb
+++ lib/will_paginate/page_number.rb
@@ -13,6 +13,7 @@ module WillPaginate
     extend Forwardable
 
     def initialize(value, name)
+      value = value.to_i if value.is_a?(String)
       value = Integer(value)
       if 'offset' == name ? (value < 0 or value > BIGINT) : value < 1
         raise RangeError, "invalid #{name}: #{value.inspect}"
