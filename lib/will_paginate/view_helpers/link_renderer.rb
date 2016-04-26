f178429060d842cebf9269e69017c0b2bbe1b179
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-04-26 16:53:08 +0200

    use old lambda syntax for ancient ruby

--- lib/will_paginate/view_helpers/link_renderer.rb
+++ lib/will_paginate/view_helpers/link_renderer.rb
@@ -119,7 +119,7 @@ module WillPaginate
           key = key.to_sym
           existing = target[key]
 
-          is_params = -> (obj) { obj.is_a?(Hash) || (defined?(ActionController::Parameters) && obj.is_a?(ActionController::Parameters)) }
+          is_params = lambda { |obj| obj.is_a?(Hash) || (defined?(ActionController::Parameters) && obj.is_a?(ActionController::Parameters)) }
 
           if is_params.call(value) and (is_params.call(existing) or existing.nil?)
             symbolized_update(existing || (target[key] = {}), value)
