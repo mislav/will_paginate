5ccb85d3bb29354e43f08f7b8e00694a3873756b
Mislav Marohnić <mislav.marohnic@gmail.com>
2020-02-26 01:04:45 +0100

    will_paginate 3.3.0

--- lib/will_paginate/version.rb
+++ lib/will_paginate/version.rb
@@ -1,8 +1,8 @@
 module WillPaginate #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 3
-    MINOR = 2
-    TINY  = 1
+    MINOR = 3
+    TINY  = 0
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
